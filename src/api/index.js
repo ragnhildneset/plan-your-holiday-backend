9dd631488403bc6cfe7ad2fc78bad9a51571b638
Duygu Akdemir <>
2017-06-07 17:50:18 +0200

    City added

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import attraction from './attraction';
+import city from './city';
 
 const router = Router();
 
 router.use('/attraction', attraction);
+router.use('/city', city);
 
 export default router;
