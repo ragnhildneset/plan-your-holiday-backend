7ede60f94e1664d6a669e60d37a06cb925c430b1
Duygu Akdemir <>
2017-06-10 20:26:02 +0200

    City added

--- src/api/city/city.controller.js
+++ src/api/city/city.controller.js
@@ -1,3 +1,13 @@
+/* eslint-disable indent,import/prefer-default-export,import/prefer-default-export */
 /**
  * Created by duyguakdemir on 07.06.17.
  */
+import City from './city.model';
+
+export function list(req, res, next) {
+  City.find()
+        .then((city) => {
+          res.json(city);
+        })
+        .catch(next);
+}
--- src/api/city/city.model.js
+++ src/api/city/city.model.js
@@ -1,3 +1,22 @@
+/* eslint-disable indent */
 /**
  * Created by duyguakdemir on 07.06.17.
  */
+import mongoose from 'mongoose';
+
+// eslint-disable-next-line indent
+const CitySchema = new mongoose.Schema({
+    name: {
+    type: String,
+    require: true
+    },
+    city: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    description: {
+        type: String
+    }
+});
+
+export default mongoose.model('City', CitySchema);
+
