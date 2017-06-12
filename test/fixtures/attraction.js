58b0dc10af5b931ca506018b9353a87045b5d377
Ragnhild Neset <ragcecnes@gmail.com>
2017-06-13 00:22:09 +0200

    update attraction mock data

--- test/fixtures/attraction.js
+++ test/fixtures/attraction.js
@@ -6,7 +6,44 @@ export default {
     {
       _id: Types.ObjectId('56aa0db353c8040f4cc54637'),
       title: 'Eisbach Surfers',
-      city: 'München',
-      address: 'Mockstrasse 5'
-    }]
+      city: Types.ObjectId('56aa0db353c8040f4cc54647'),
+      address: 'Mockstrasse 5',
+      duration: 120,
+      coordinates: {
+        longitude: 10000,
+        latitude: 20000
+      },
+      rating: {
+        quality: {
+          rating: 4.7,
+          amount: 3000
+        },
+        popularity: {
+          rating: 4.3,
+          amount: 3000
+        }
+      }
+    },
+    {
+      _id: Types.ObjectId('56aa0db353c8040f4cc54537'),
+      title: 'Marienplatz',
+      city: Types.ObjectId('56aa0db353c8040f4cc55647'),
+      address: 'Mockstrasse 7',
+      duration: 20,
+      coordinates: {
+        longitude: 10000,
+        latitude: 20000
+      },
+      rating: {
+        quality: {
+          rating: 4.2,
+          amount: 3000
+        },
+        popularity: {
+          rating: 4.8,
+          amount: 3000
+        }
+      }
+    }
+  ]
 };
