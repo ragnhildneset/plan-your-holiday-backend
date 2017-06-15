d969b96fdd8c05dd7501b67f3d4f534a9319a6dd
Sael Bayona <sael.bayona@tum.de>
2017-06-15 23:15:00 +0200

    Including Backend for Users

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import attraction from './attraction';
-import user from './user';
+
 
 const router = Router();
 
+var passport = require('passport');
+var jwtConfig = require('./passport/jwtConfig');
+
+/*SB: Routers*/
+
+var userRoutes = require("./user/index");
 router.use('/attractions', attraction);
-router.use('/user', user);
+router.use('/user', userRoutes(passport));
+
+
+
 
 export default router;
